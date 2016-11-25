31289bda799f5ac5dacf970b014785ee30ccfae3
arwinneil <arwinneil@gmail.com>
2016-11-25 13:01:55 +0400

    Installed MaterialSkin NuGet Package

--- raw2thumb/Form1.vb
+++ raw2thumb/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Imports MaterialSkin
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
+        SkinManager.AddFormToManage(Me)
+        SkinManager.Theme = MaterialSkinManager.Themes.LIGHT
+        SkinManager.ColorScheme = New ColorScheme(Primary.Cyan300, Primary.Cyan400, Primary.Cyan200, Accent.LightBlue200, TextShade.WHITE)
+    End Sub
+
 
 End Class
